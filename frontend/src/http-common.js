b0cf9395eede26210994cc6fca1c6cea92496de7
모로리 <gusdn6082@naver.com>
2021-09-20 02:08:57 +0900

    cors 관련 수정

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://ec2-15-152-46-45.ap-northeast-3.compute.amazonaws.com:8080/api",
+    baseURL: "http://ec2-13-208-175-95.ap-northeast-3.compute.amazonaws.com/api",
     header: {
         "Content-type": "application/json; charset = utf-8"
     }
