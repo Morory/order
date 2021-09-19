dc293a4e24c3d4a2324550edbd4ab2a393abcbac
모로리 <gusdn6082@naver.com>
2021-09-19 23:46:00 +0900

    cors 관련 수정

--- src/main/java/com/management/order/security/WebConfig.java
+++ src/main/java/com/management/order/security/WebConfig.java
@@ -9,12 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("http://ec2-15-152-34-71.ap-northeast-3.compute.amazonaws.com:8080/")
-                .allowedOrigins("http://ec2-15-152-34-71.ap-northeast-3.compute.amazonaws.com:8080")
-                .allowedOrigins("http://localhost:8080")
-                .allowCredentials(true);
+        registry.addMapping("/**");
     }
 }
